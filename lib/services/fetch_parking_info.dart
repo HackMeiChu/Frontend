15b75fae91556ee6341c7db8a68882e8d3a7c41e
RitaHsieh <ritasha356@gmail.com>
2024-10-20 11:51:30 +0800

    target parking lots

--- lib/services/fetch_parking_info.dart
+++ lib/services/fetch_parking_info.dart
@@ -1,20 +1,58 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:transportation/models/parking_info_model.dart';
 
 const baseUrl = 'parkinglot-backend.onrender.com';
 
 class FetchDataService extends ChangeNotifier {
+  GoogleMapController? mapController;
+  final List<ParkinglotInfo> parkinglotInfo = [];
+  final List<ParkinglotPredSpace> parkinglotPredSpace_list = [];
+  Set<Marker> marker = {};
+  LatLng? center;
+  Position? currentPosition;
 
-  final List<ParkinglotInfo> parkinglogInfo = [];
-  final List<ParkinglotPredSpace> parkinglotPredSpaces = [];
-  int counter = 0;
-
-  void count(){
-    counter ++;
-    notifyListeners();
+  FetchDataService(){
+    getUserLocation();
+    getParkingInfo();
   }
+  void onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+
+  void getUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+    // Request permission to get the user's location
+    permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return;
+      }
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+        permission != LocationPermission.always) {
+        return;
+      }
+    }
+    // Get the current location of the user
+    currentPosition = await Geolocator.getCurrentPosition();
+    // setState(() {
+      // log("in set state");
+    center = LatLng(currentPosition!.latitude, currentPosition!.longitude);
+    notifyListeners();
+    // });
+  }
+
 
   Future<void> getParkingInfo() async {
     final parkingInfoUrl = Uri.https(baseUrl, 'parking');
@@ -29,27 +67,53 @@ class FetchDataService extends ChangeNotifier {
     List<ParkinglotInfo> parkingInfo = List<ParkinglotInfo>.from(
         resJson.map((model) => ParkinglotInfo.fromJson(model)));
     // return parkingInfo;
-    parkinglogInfo.addAll(parkingInfo);
-    notifyListeners();
+    parkinglotInfo.addAll(parkingInfo);
+    // notifyListeners();
   }
+    
 
   Future<void> getNearbyPredictParkingSpe(
       double lat, double lng, int minutes) async {
     final parkingInfoUrl =
-        Uri.https(baseUrl, 'parking/predict', {"lat": lat, "lng": lng, "minutes": minutes});
+        Uri.https(baseUrl, 'parking/predict', {"lat": '$lat', "lng": '$lng', "minutes": '$minutes'});
 
     var res = await http.get(parkingInfoUrl);
     if (res.statusCode != 200) {
       print("cannot fetch from the api");
       // return []; 
     }
-
+   
     final resJson = jsonDecode(res.body);
     List<ParkinglotPredSpace> parkinglotPredSpaces = List<ParkinglotPredSpace>.from(
         resJson.map((model) => ParkinglotPredSpace.fromJson(model)));
     // return parkinglotPredSpaces;
-    parkinglotPredSpaces.addAll(parkinglotPredSpaces);
+    parkinglotPredSpace_list.addAll(parkinglotPredSpaces);
+    center = LatLng(lat, lng);
+    mapController!.animateCamera(CameraUpdate.newLatLngZoom(center!, 17));
+    toMarker();
+    // notifyListeners();
+  }
+
+
+  void toMarker() {
+
+    for(ParkinglotPredSpace item in parkinglotPredSpace_list) {
+        for (ParkinglotInfo parkinglot in parkinglotInfo) {
+          if(parkinglot.id == item.parkinglot_id) {
+             marker.add(
+              Marker(
+              markerId: MarkerId(parkinglot.id.toString()),
+              position: LatLng(parkinglot.latitude, parkinglot.longitude),
+              )
+            );
+
+          }
+        }
+    }
     notifyListeners();
   }
+
+   
+  
 
 }
